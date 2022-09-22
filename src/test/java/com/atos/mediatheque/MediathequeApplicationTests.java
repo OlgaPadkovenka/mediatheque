779da0f38515125cec84536049de35dd1641a9b9
Olga <olga.padkovenka.simplon@gmail.com>
2022-09-22 10:05:27 +0200

    initialisation du project

---
+++ src/test/java/com/atos/mediatheque/MediathequeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atos.mediatheque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediathequeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
