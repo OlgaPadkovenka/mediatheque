7f65e5e766ba0078614723cd3ad914a019672209
Olga <olga.padkovenka.simplon@gmail.com>
2022-10-05 15:15:28 +0200

    add thymeleaf & bootstrap

--- src/main/java/com/atos/mediatheque/repository/UtilisateurRerository.java
+++ src/main/java/com/atos/mediatheque/repository/UtilisateurRerository.java
@@ -1,11 +1,20 @@
 package com.atos.mediatheque.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.atos.mediatheque.entity.Utilisateur;
 
 public interface UtilisateurRerository extends JpaRepository<Utilisateur, Long> {
 	
 	void deleteByEmail(String email);
+	
+	////la requete pour un update de l'utilisateur recherché par email
+	//@Modifying
+	//@Query("update Utilisateur u set where u.email = :email")
+	//void updateUtilisateur(@Param(value="email") String email);
 
 }
+ 
