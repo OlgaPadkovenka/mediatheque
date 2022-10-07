d3a7b1cc61ef0cf7e3108ff91300b0eebe661204
Olga <olga.padkovenka.simplon@gmail.com>
2022-10-07 12:26:08 +0200

    add import.sql pour Utilisateur

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,3 +3,9 @@ insert into CD (ID, NOM, DUREE, NOMBRE_D_EXEMPLAIRES, DATE_DE_PARUTION,TITRE) va
 insert into CD (ID, NOM, DUREE, NOMBRE_D_EXEMPLAIRES, DATE_DE_PARUTION,TITRE) values (3,'Aaliyah', 73, 18,'1996-08-13','One in a million');
 insert into CD (ID, NOM, DUREE, NOMBRE_D_EXEMPLAIRES, DATE_DE_PARUTION,TITRE) values (4,'D Angelo', 78, 13,'2000-01-25','Voodoo');
 insert into CD (ID, NOM, DUREE, NOMBRE_D_EXEMPLAIRES, DATE_DE_PARUTION,TITRE) values (5,'Nas', 60, 18,'2006-12-19','HipHop is Dead');
+
+insert into UTILISATEUR (ID, NOM, PRENOM, EMAIL, MOT_DE_PASSE) values (1,'Fleurisse','Atangana','bkzion@hotmail.fr','laforce');
+insert into UTILISATEUR (ID, NOM, PRENOM, EMAIL, MOT_DE_PASSE) values (2,'Sylvie','Wagner','syzion@hotmail.fr','lesecret');
+insert into UTILISATEUR (ID, NOM, PRENOM, EMAIL, MOT_DE_PASSE) values (3,'Roger','Smith','smithrog@hotmail.fr','unpassword');
+insert into UTILISATEUR (ID, NOM, PRENOM, EMAIL, MOT_DE_PASSE) values (4,'Erik','Jackson','jacksEr@hotmail.fr','unknown');
+insert into UTILISATEUR (ID, NOM, PRENOM, EMAIL, MOT_DE_PASSE) values (5,'Daniela','Sagna','sagnaD@hotmail.fr','flawless');
