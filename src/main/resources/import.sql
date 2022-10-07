b23aece9d531b8b02f0fa075e8f80211ca288400
Olga <olga.padkovenka.simplon@gmail.com>
2022-10-07 12:16:08 +0200

    add import.sql pour CD

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into CD (ID, NOM, DUREE, NOMBRE_D_EXEMPLAIRES, DATE_DE_PARUTION,TITRE) values (1,'Kanye West', 125, 20,'2003-12-02','The college Dropout');
+insert into CD (ID, NOM, DUREE, NOMBRE_D_EXEMPLAIRES, DATE_DE_PARUTION,TITRE) values (2,'Jay Z', 120, 54,'2003-11-14','The Black Album');
+insert into CD (ID, NOM, DUREE, NOMBRE_D_EXEMPLAIRES, DATE_DE_PARUTION,TITRE) values (3,'Aaliyah', 73, 18,'1996-08-13','One in a million');
+insert into CD (ID, NOM, DUREE, NOMBRE_D_EXEMPLAIRES, DATE_DE_PARUTION,TITRE) values (4,'D Angelo', 78, 13,'2000-01-25','Voodoo');
+insert into CD (ID, NOM, DUREE, NOMBRE_D_EXEMPLAIRES, DATE_DE_PARUTION,TITRE) values (5,'Nas', 60, 18,'2006-12-19','HipHop is Dead');
