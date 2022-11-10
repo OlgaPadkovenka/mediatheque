e5621abf9748bbaa6bd398b4d1738fcc26296b01
olga.padkovenka <olga.padkovenka.simplon@gmail.com>
2022-11-10 14:25:42 +0100

    insert card and navbar to index

--- src/main/java/com/atos/mediatheque/web/WebController.java
+++ src/main/java/com/atos/mediatheque/web/WebController.java
@@ -9,12 +9,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.atos.mediatheque.entity.Item;
 import com.atos.mediatheque.repository.ItemRepository;
+import com.atos.mediatheque.repository.UtilisateurRerository;
 
 @Controller
 public class WebController {
 	
 	@Autowired
 	private ItemRepository itemRepository;
+	
+	@Autowired
+	private UtilisateurRerository utilisateurRerository;
 
 	@GetMapping("/")
 	public String index(Model model) {
@@ -23,8 +27,11 @@ public class WebController {
 		return "index";
 	}
 	
-	@GetMapping("/items")
-	public String items() {
-		return "items";
+	
+	@GetMapping("/user/{id}")
+	public String user(Model model) {
+		List<Item> listItems = itemRepository.findAll();
+		model.addAttribute("listItems", listItems);
+		return "user";
 	}
 }
