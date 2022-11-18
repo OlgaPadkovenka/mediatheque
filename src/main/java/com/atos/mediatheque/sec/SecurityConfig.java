ef98e998a96cd7168c0d9063ec95749afee55b43
olga.padkovenka <olga.padkovenka.simplon@gmail.com>
2022-11-18 16:05:52 +0100

    security customized without WebSecurityConfigurerAdapter

--- src/main/java/com/atos/mediatheque/sec/SecurityConfig.java
+++ src/main/java/com/atos/mediatheque/sec/SecurityConfig.java
@@ -1,26 +1,27 @@
 package com.atos.mediatheque.sec;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
-
-@EnableWebSecurity
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
-	
-
-	@Autowired
-public void  configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-
-	 auth.inMemoryAuthentication().withUser("user").password(passwordEncoder().encode("password")).roles("USER");
+public class SecurityConfig {
+		
+	@Bean
+public InMemoryUserDetailsManager userDetailsService() {
+		
+		  UserDetails user = User
+		            .withUsername("user")
+		            .password(passwordEncoder().encode("password"))
+		            .roles("USER")
+		            .build();
+		return new InMemoryUserDetailsManager(user);
 
 }
 	
@@ -30,11 +31,23 @@ public void  configureGlobal(AuthenticationManagerBuilder auth) throws Exception
 	   }
 	 
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		  http.authorizeRequests()
-	      .anyRequest().authenticated()
-	      .and().formLogin();
+	@Bean
+	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+		
+        http
+        .authorizeHttpRequests((authz) -> {
+			try {
+				authz
+				    .anyRequest().authenticated()
+				    .and().formLogin();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+        );
+    return http.build();
+		
 	} 
 	
 
